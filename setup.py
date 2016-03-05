d58a7b9f2fb61ed737b13382e59722d7c37265d4
Jeff Quast <contact@jeffquast.com>
2016-03-05 11:42:57 -0800

    some minor polish

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HERE = os.path.dirname(__file__)
 
 setuptools.setup(
     name='qwack',
-    version=0.0.1,
+    version='0.0.2',
     install_requires=['pyyaml', 'blessed'],
     long_description=open(os.path.join(HERE, 'README.rst')).read(),
     description='a quickly written hack (1985) variant for Python.',
