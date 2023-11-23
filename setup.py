7744b6c79d66eed7ae41c0694394e68ee90772f9
Jeff Quast <contact@jeffquast.com>
2023-11-22 22:06:33 -0500

    narfie

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ HERE = os.path.dirname(__file__)
 
 setuptools.setup(
     name='qwack',
-    version='0.0.3',
+    version='0.0.4',
     install_requires=['pyyaml', 'blessed'],
     long_description=open(os.path.join(HERE, 'README.rst')).read(),
     description='a rogue-like game of mysterious origins!',
     author='Jeff Quast',
     author_email='contact@jeffquast.com',
     license='MIT',
-    packages=['qwack'],
+    packages=['qwack.dat'],
     # just add the tilesets and world.yaml so far ..
     package_data={"dat": ["*.zip", "*.yaml", "*.ULT", "*.MAP"]},
     url='https://github.com/jquast/qwack',
